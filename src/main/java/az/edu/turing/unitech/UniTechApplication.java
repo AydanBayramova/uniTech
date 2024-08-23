ab00d9219b3ba4d8397aa6b390f5a86407fd6a5c
AydanBayramova <bayramoffaaydan1@gmail.com>
2024-08-23 15:23:03 +0400

    UniTech project

---
+++ src/main/java/az/edu/turing/unitech/UniTechApplication.java
@@ -0,0 +1,13 @@
+package az.edu.turing.unitech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniTechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/az/edu/turing/unitech/UniTechApplicationTests.java
@@ -0,0 +1,13 @@
+package az.edu.turing.unitech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
