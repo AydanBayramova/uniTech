09485dc7c3d3ff3e769aabdd215d360f7deaf193
AydanBayramova <bayramoffaaydan1@gmail.com>
2024-09-01 18:41:21 +0400

    write abstract methods and create UserService interfaces

--- src/main/java/az/edu/turing/unitech/service/UserService.java
+++ src/main/java/az/edu/turing/unitech/service/UserService.java
@@ -1,12 +1,27 @@
 package az.edu.turing.unitech.service;
 
 import az.edu.turing.unitech.domain.repository.UserRepository;
+import az.edu.turing.unitech.model.dto.UserDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-@Service
-@RequiredArgsConstructor
-public class UserService {
-    private final UserRepository userRepository;
+import java.util.Optional;
 
+@Service
+
+public interface UserService {
+
+    Optional<UserDto> getUserById(UserDto userDto);
+
+    Page<UserDto> getAll(Pageable pageable);
+
+    UserDto update(Long id, UserDto userDto);
+
+    UserDto createUser(UserDto userDto);
+
+    void deleteById(Long id);
+
+    void deleteAll();
 }
