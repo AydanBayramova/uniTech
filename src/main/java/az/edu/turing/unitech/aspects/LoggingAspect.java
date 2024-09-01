8bf933e831da7ed85901b894e7bc6cebedd05d6d
AydanBayramova <bayramoffaaydan1@gmail.com>
2024-09-01 16:12:42 +0400

    Use Aspect annotation for use  AOP programming and create LoggingAspect class

---
+++ src/main/java/az/edu/turing/unitech/aspects/LoggingAspect.java
@@ -0,0 +1,9 @@
+package az.edu.turing.unitech.aspects;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LoggingAspect {
+}
