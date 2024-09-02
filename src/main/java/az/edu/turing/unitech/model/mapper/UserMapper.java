94c8d3d7e63a9fceec2d93e77e0f6c8f80d8b8db
AydanBayramova <bayramoffaaydan1@gmail.com>
2024-09-02 15:11:34 +0400

    Create UserMapper interface and use mapstruct , such as mapper annotation and dto to entity, or entity to dto

---
+++ src/main/java/az/edu/turing/unitech/model/mapper/UserMapper.java
@@ -0,0 +1,22 @@
+package az.edu.turing.unitech.model.mapper;
+
+import az.edu.turing.unitech.domain.entity.UserEntity;
+import az.edu.turing.unitech.model.dto.UserDto;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface UserMapper {
+
+    UserDto userDtoToEntity(UserEntity userEntity);
+
+    UserEntity userEntityToDto(UserDto userDto);
+
+    List<UserDto> userEntityListToDtoList(List<UserEntity> userEntities);
+
+    List<UserEntity> userDtoListToEntityList(List<UserDto> userDtos);
+
+    void updatedDtoFromEntity(UserDto userDto, @MappingTarget UserEntity userEntity);
+}
