4790d20d8670e98a621667210ee78bc7dae00bbd
AydanBayramova <bayramoffaaydan1@gmail.com>
2024-09-08 16:33:37 +0400

    Create Login class

--- src/main/java/az/edu/turing/unitech/service/LoginService.java
+++ src/main/java/az/edu/turing/unitech/service/LoginService.java
@@ -1,8 +1,15 @@
 package az.edu.turing.unitech.service;
 
+import az.edu.turing.unitech.config.JwtTokenProvider;
+import az.edu.turing.unitech.domain.entity.AccountEntity;
+import az.edu.turing.unitech.domain.entity.UserEntity;
 import az.edu.turing.unitech.domain.repository.AccountRepository;
 import az.edu.turing.unitech.domain.repository.UserRepository;
+import az.edu.turing.unitech.model.dto.LoginRequest;
+import az.edu.turing.unitech.model.dto.LoginResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -13,7 +20,26 @@ public class LoginService {
     private final UserRepository userRepository;
     private final AccountRepository accountRepository;
     private final PasswordEncoder passwordEncoder;
+    private final JwtTokenProvider jwtTokenProvider;
+
+    public LoginResponse login(LoginRequest loginRequest) {
+
+        UserEntity user = userRepository.findByPin(loginRequest.getPin())
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
 
 
+        AccountEntity account = accountRepository.findByAccountNumber(loginRequest.getAccountNumber())
+                .orElseThrow(() -> new BadCredentialsException("Account not found"));
 
+
+        if (!passwordEncoder.matches(loginRequest.getPassword(), account.getPassword())) {
+            throw new BadCredentialsException("Invalid password");
+        }
+
+
+        String token = jwtTokenProvider.createToken(user.getPin());
+
+
+        return new LoginResponse(token);
+    }
 }
