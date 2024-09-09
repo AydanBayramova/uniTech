5560e4d032b770fa3638605d01897fe4344e6dc5
AydanBayramova <bayramoffaaydan1@gmail.com>
2024-09-09 14:08:15 +0400

    update security settings

--- src/main/java/az/edu/turing/unitech/service/impl/NotificationImpl.java
+++ src/main/java/az/edu/turing/unitech/service/impl/NotificationImpl.java
@@ -7,9 +7,11 @@ import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
 
 
 @RequiredArgsConstructor
+@Service
 public class NotificationImpl implements Notification {
 
     private final JavaMailSender mailSender;
